c92201fdd546c79716c8732f6508501441a8603f
Lionel Rowe <lionel.rowe@gmail.com>
2019-03-25 03:49:37 +0800

    optimize detection rules

--- src/detectCode.js
+++ src/detectCode.js
@@ -2,14 +2,13 @@
 
 const stripWrappedCode = content => {
   const codeTypes = [
-    /(^`{3,}).*\r?\n[\s\S]*\r?\n\1/gm, // backtick-fenced block
-    /(^~{3,}).*\r?\n[\s\S]*\r?\n\1/gm, // tilde-fenced block
+    /(^([`~])\2{2,}).*\r?\n[\s\S]*?\r?\n\1\2*\s*(?:\r?\n|$)/gm, // backtick-/tilde-fenced block
     /(?:^|(?:\r?\n{2,}))\s*(?:(?: {4}|\t).*(?:\r?\n|$))/g, // indented block
     // lack of `m` flag is intentional (`^` must match beginning of input, not line)
 
-    /\[code.*\][\s\S]*\[\/code\]/gm, // BBCode tags
+    /\[code(?: [^\]\r\n])\][\s\S]*\[\/code\]/gm, // BBCode tags
 
-    /`.+`/g, // inline backticks (must come last)
+    /`[^`\r\n]+`/g, // inline backticks (must come last)
   ];
 
   const strippedContent = codeTypes.reduce((str, codeType) => {
@@ -25,30 +24,36 @@ const varNameStart = '[$_a-zA-Z]';
 const varNameEnd = '[$_a-zA-Z0-9]*';
 const varName = `${varNameStart}${varNameEnd}`;
 const xmlLikeName = '[a-zA-Z-]+';
+const string = `(?:"(?:[^\\r\\n"\\\\]|\\\\[^\\r\\n])*"|'(?:[^\\r\\n'\\\\]|\\\\[^\\r\\n])*')`;
+// adapted from http://wordaligned.org/articles/string-literals-and-regular-expressions
+const numeric = '-?(?:0|[1-9]\\d*)(?:\\.\\d+)?(?:[eE][+-]?\\d+)?';
+// adapted from https://www.json.org/
+const boolean = '(?:true|false)';
+const argument = `(?:${varName}|${string}|${numeric}|${boolean}|null)`;
 
 const nonHtmlIndicators = [
-  `[$_]${varName}`, // almost certain to be var name
+  `\\$${varName}`, // almost certain to be var name
+  // omitted: _varName (conflict with italics)
   `${varName}(?:_${varName})+`, // snake_case
-  // camelCase and spinal-case omitted due to too many false positives
+  // ommitted: camelCase and spinal-case (too many false positives)
   '(?:^|\\s+)(?:\\/\\/|;)', // single-line comment
-  // ignore python-style `#` single-line comments due to conflict with md headings
-  `\\/\\*[\\s\\S]+\\*\\/`, // C-like multiline comment
-  `('''|""")[\\s\\S]+\\1`, // python-like multiline string/comment
+  // omitted: python-style `#` single-line comments (conflict with md headings)
+  `\\/\\*[\\s\\S]+?\\*\\/`, // C-like multiline comment
+  `('''|""")[\\s\\S]+?\\1`, // python-like multiline string/comment
   ';\\s*$', // trailing semicolon
-  `${varName}\\((?:${varName})?\\)`, // function call
-  `${varName}\\[(${varName}?)\\]`, // array index
+  `${varName}\\(\\s*${argument}?\\s*\\)`, // function call
+  `${varName}\\[\\s*${argument}?\\s*\\]`, // array index
   `${varName}\\.${varName}`, // object property
   '^\\s*[{}]\\s*$', // curly brace and nothing else on a line
   '\\{\\{.+\\}\\}', // templating languages e.g. handlebars
   '[$#]\\{.+\\}', // template string
-  '&&|\\|\\||!=|>=|<=|=>|->|>>|<<|::'
-  + '|\\+\\+|\\+=|-=|\\*=|\\/=|\\|=|&=|\\?=', // various operators
+  '&&|\\|\\||!=|>>|<<|::|\\+\\+|\\+=|-=|\\*=|\\/=|\\|=|&=|\\?=', // various operators
   '\\\\[\'"ntr0\\\\]', // common escape sequences
 ];
 
 const htmlIndicators = [
   '<!--[\\s\\S]*?-->', // xml-like comment
-  `<${xmlLikeName}.*\\/?>`, // xml-like start/empty tag
+  `<${xmlLikeName}[^>]*\\/?>`, // xml-like start/empty tag
   `</${xmlLikeName}>`, // xml-like end tag
   '&([0-9a-zA-Z]+);$', // html entity - human-readable
   '&#([0-9]{1,7});$', // html entity - decimal
