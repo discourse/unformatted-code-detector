cfa69b63f643e43e80272f1f44b3af9b527479f3
lionel-rowe <lionel.rowe@gmail.com>
2021-05-17 15:13:42 +0100

    Standardize file names and dir structure

--- __test__/detect-code/without-html.test.js
+++ __test__/detect-code/without-html.test.js
@@ -1,12 +1,12 @@
-import { expectAll } from "./utils";
-import { withBareHTML } from "./fixtures";
+import { expectAll } from "../utils";
+import { withBareHTML } from "../fixtures";
 
 describe("Without HTML", () => {
   jest.resetModules();
   global.settings = { include_html: false, matches_to_ignore: 0 };
   const {
     detectUnformattedCode,
-  } = require("../javascripts/unformatted_code_detector/lib/detectCode.js.es6");
+  } = require("../../javascripts/unformatted_code_detector/lib/detect-code.js.es6");
 
   expectAll(detectUnformattedCode, withBareHTML, false);
 });
