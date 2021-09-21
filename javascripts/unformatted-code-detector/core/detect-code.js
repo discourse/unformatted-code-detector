ebb9177c51b9dec86d2f712d0e503847c3343658
David Taylor <david@taylorhq.com>
2021-09-21 11:50:55 +0100

    FEATURE: Add helper function to print debug output (#40)
    To see debug output, open the composer, then type `debugUnformattedCodeDetector()` in the console.

--- javascripts/unformatted-code-detector/core/detect-code.js
+++ javascripts/unformatted-code-detector/core/detect-code.js
@@ -13,7 +13,10 @@ export const getCodeEnergy = (content) => {
 
   const lines = getLineBoundaries(content);
 
-  lines.forEach((x) => (x.matches = 0));
+  lines.forEach((x) => {
+    x.matches = 0;
+    x.matched_patterns = [];
+  });
 
   for (const { matcher, value } of getCodeEnergyIndicators()) {
     const matches = [...content.matchAll(matcher)];
@@ -37,6 +40,7 @@ export const getCodeEnergy = (content) => {
           (line.start >= startIndex && line.end <= endIndex)
         ) {
           ++line.matches;
+          line.matched_patterns.push({ matcher, value });
         }
       }
     }
@@ -61,9 +65,65 @@ export const numSequentialLinesWithThresholdCodeEnergy = (threshold) => (
       maxContiguous = Math.max(maxContiguous, curContiguous);
       curContiguous = 0;
     }
-  }
 
-  return Math.max(maxContiguous, curContiguous);
+    return Math.max(maxContiguous, curContiguous);
+  };
+
+export const printDebugInfo = (content) => {
+  content = stripIgnoredContent(content);
+
+  const {
+    complexMatchesToIgnore,
+    minSequentialLinesToMatch,
+    minTotalCodeEnergy,
+  } = sensitivityConfig;
+
+  const { totalCodeEnergy, totalComplexMatches, lines } =
+    getCodeEnergy(content);
+
+  const debugTable = [];
+
+  let consecutive = 0;
+  lines.forEach((l) => {
+    if (!l.content.trim()) return;
+    if (l.matches) {
+      consecutive++;
+    } else {
+      consecutive = 0;
+    }
+    debugTable.push([
+      l.matches ? String(l.matches) : "",
+      consecutive ? String(consecutive) : "",
+      l.matched_patterns.length
+        ? `\`${l.matched_patterns.map((p) => p.matcher).join("`, `")}\``
+        : "",
+      l.content,
+    ]);
+  });
+
+  const columns = ["matches", "cumulative", "matched patterns", "content"];
+  const dividers = columns.map(() => "");
+
+  columns.forEach((c, i) => {
+    const longest = Math.max(
+      c.length,
+      ...debugTable.map((row) => row[i].length)
+    );
+    debugTable.forEach((row) => (row[i] = row[i].padEnd(longest)));
+    columns[i] = c.padEnd(longest);
+    dividers[i] = dividers[i].padEnd(longest, "-");
+  });
+
+  debugTable.unshift(columns, dividers);
+
+  console.log(debugTable.map((l) => `| ${l.join(" | ")} |`).join("\n"));
+
+  console.log("Result is ", { totalCodeEnergy, totalComplexMatches, lines });
+  console.log("Sensitivity Config is ", {
+    complexMatchesToIgnore,
+    minSequentialLinesToMatch,
+    minTotalCodeEnergy,
+  });
 };
 
 const detectCode = (content) => {
