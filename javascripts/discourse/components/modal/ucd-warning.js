90500f04973d7657787809931cbc54ddc4db0261
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2023-08-14 14:38:38 +0200

    linting

--- javascripts/discourse/components/modal/ucd-warning.js
+++ javascripts/discourse/components/modal/ucd-warning.js
@@ -1,6 +1,5 @@
 import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
-import Controller from "@ember/controller";
 import Component from "@glimmer/component";
 
 export default class ModalUcdWarning extends Component {
