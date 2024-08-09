faa1317cf8269655c339830014937a336d522cec
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2024-08-09 10:09:45 +0200

    Update javascripts/discourse/initializers/init.js
    Co-authored-by: David Taylor <david@taylorhq.com>

--- javascripts/discourse/initializers/init.js
+++ javascripts/discourse/initializers/init.js
@@ -47,7 +47,7 @@ export default {
       );
 
       api.modifyClass(
-        "controller:composer",
+        "service:composer",
         (Superclass) =>
           class extends Superclass {
             save(...args) {
